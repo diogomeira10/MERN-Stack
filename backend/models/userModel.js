7393e613234fe51ad3efe3ba30e599fc27df3396
Diogo Meira <diogomeira81@gmail.com>
2024-03-10 23:04:21 +0000

    start auth with bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema;
 
@@ -16,6 +17,30 @@ const userSchema = new Schema(
 
     }
 )
+
+
+ //static signup method
+ //We can later call this signup function whenver we want to signup a new User
+ //When we create a static method like this instead of using the model name , we can just reference it by using the 'this' keyword.
+ userSchema.statics.signup = async function (email, password) {
+    const exists = await this.findOne({email}) //to be able to use the this keyword, it has to be inside a regular function, not an arrow function
+
+    if(exists) {
+        throw Error('Email already in use')
+    }
+
+    // mypasswordj87123sf125fg
+
+    const salt = await bcrypt.genSalt(10) //this generates the sault, wich is an extra layer of protection matching of passwords
+
+    //Now we hash the salt with the password
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({email, password: hash})
+
+    return user
+
+}
 
 
 module.exports = mongoose.model('User', userSchema)
